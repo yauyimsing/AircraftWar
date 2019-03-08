19bf6f00ded8497ab0d63013a4301dd42b6063c6
yau linux <yauyimsing@gmail.com>
2019-03-08 15:23:55 +0800

    extract base class

--- AircraftWar.py
+++ AircraftWar.py
@@ -5,14 +5,17 @@ import time
 from pygame.locals import *
 import random
 #
-class HeroPlane(object):
-    def __init__(self, screen_temp):
-        self.x = 210
-        self.y = 600
+class Base(object):
+    def __init__(self, screen_temp, x, y, image_name):
+        self.x = x
+        self.y = y
         self.screen = screen_temp
-        self.image = pygame.image.load("./feiji/hero1.png")
+        self.image = pygame.image.load(image_name)
+        
+class BasePlane(Base):
+    def __init__(self, screen_temp, x, y, image_name):
+        Base.__init__(self, screen_temp, x, y, image_name)
         self.bullet_list = []
-
     def display(self):
         self.screen.blit(self.image, (self.x, self.y))
         del_bullet_list = []
@@ -23,6 +26,37 @@ class HeroPlane(object):
                 del_bullet_list.append(bullet)
         for del_bullet in del_bullet_list:
             self.bullet_list.remove(del_bullet)
+
+class HeroPlane(BasePlane):
+    def __init__(self, screen_temp):
+        BasePlane.__init__(self, screen_temp, 210, 600, "./feiji/hero1.png") 
+        self.hit = False
+        self.bomb_list = []
+        self.__create_images()
+        self.image_num = 0
+        self.image_index = 0
+    def display(self):
+        if self.hit == True:
+            self.screen.blit(self.bomb_list[self.image_index], (self.x, self.y))
+            self.image_num += 1
+            if self.image_num == 7:
+                self.image_num = 0
+                self.image_index += 1
+            if self.image_index > 3:
+                time.sleep(1)
+                exit()
+        else:
+            self.screen.blit(self.image, (self.x, self.y))
+        del_bullet_list = []
+        for bullet in self.bullet_list:
+            bullet.display()
+            bullet.move()
+            if bullet.judge(): # judge if the bullet out of bounder
+                del_bullet_list.append(bullet)
+        for del_bullet in del_bullet_list:
+            self.bullet_list.remove(del_bullet)
+    def bomb(self):
+        self.hit = True
 
     def move_left(self):
         self.x -= 5
@@ -38,26 +72,16 @@ class HeroPlane(object):
 
     def fire(self):
         self.bullet_list.append(Bullet(self.screen, self.x, self.y))
+    def __create_images(self):
+        self.bomb_list.append(pygame.image.load("./feiji/hero_blowup_n1.png"))
+        self.bomb_list.append(pygame.image.load("./feiji/hero_blowup_n2.png"))
+        self.bomb_list.append(pygame.image.load("./feiji/hero_blowup_n3.png"))
+        self.bomb_list.append(pygame.image.load("./feiji/hero_blowup_n4.png"))
 #
-class EnemyPlane(object):
+class EnemyPlane(BasePlane):
     def __init__(self, screen_temp):
-        self.x = 0
-        self.y = 0
-        self.screen = screen_temp
-        self.image = pygame.image.load("./feiji/enemy0.png")
-        self.bullet_list = []
+        BasePlane.__init__(self, screen_temp, 0, 0, "./feiji/enemy0.png")
         self.direction = "right"
-
-    def display(self):
-        self.screen.blit(self.image, (self.x, self.y))
-        del_bullet_list = []
-        for bullet in self.bullet_list:
-            bullet.display()
-            bullet.move()
-            if bullet.judge(): # judge if the bullet out of bounder
-                del_bullet_list.append(bullet)
-        for del_bullet in del_bullet_list:
-            self.bullet_list.remove(del_bullet)
 
     def move(self):
         if self.direction == "right":
@@ -74,33 +98,28 @@ class EnemyPlane(object):
         if random_num == 1 or random_num == 50:
             self.bullet_list.append(EnemyBullet(self.screen, self.x, self.y))
 
-class Bullet(object):
-    def __init__(self, screen_temp, x, y):
-        self.x = x + 40
-        self.y = y - 20
-        self.screen = screen_temp
-        self.image = pygame.image.load("./feiji/bullet.png")
+class BaseBullet(Base):
+    def __init__(self, screen_temp, x, y, image_name):        
+        Base.__init__(self, screen_temp, x, y, image_name)
 
     def display(self):
         self.screen.blit(self.image, (self.x, self.y))
 
+class Bullet(BaseBullet):
+    def __init__(self, screen_temp, x, y):
+        BaseBullet.__init__(self, screen_temp, x+40, y-20, "./feiji/bullet.png")
+
     def move(self):
-        self.y -= 5
+        self.y -= 10
 
     def judge(self):
         if self.y < 0:
             return True
         else:
             return False
-class EnemyBullet(object):
+class EnemyBullet(BaseBullet):
     def __init__(self, screen_temp, x, y):
-        self.x = x + 25
-        self.y = y + 40
-        self.screen = screen_temp
-        self.image = pygame.image.load("./feiji/bullet1.png")
-
-    def display(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        BaseBullet.__init__(self, screen_temp, x+25, y+40, "./feiji/bullet1.png")
 
     def move(self):
         self.y += 5
@@ -129,6 +148,11 @@ def key_control(hero_temp):
                 hero_temp.move_down()
             elif event.key == K_SPACE:
                 hero_temp.fire()
+            elif event.key == K_ESCAPE:
+                print('key escape')
+                exit()
+            elif event.key == K_b:
+                hero_temp.bomb()
             else:
                 pass
 
